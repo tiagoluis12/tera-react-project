75a4c38d4d9cf5580e4bf60aa162848707f7fe8d
“tiagoluis12” <tiago7164@hotmail.com>
2023-09-01 01:58:32 +0100

    migrated HTML from remaining screens

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+// import Home from './components/pages/Home'
+// import Users from './components/pages/Users'
+// import UserBlog from './components/pages/UserBlog'
+import PostForm from './components/pages/PostForm'
+
+import "./styles/normalize.css"
+import "./styles/fontawesome.min.css"
+import "./styles/main.css"
+
 function App() {
-  return <div>Projeto</div>
+  return <PostForm />
 }
 
 export default App;
