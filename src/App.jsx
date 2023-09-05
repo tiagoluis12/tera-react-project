7f820229add80f393c13f007e106470f21adc873
“tiagoluis12” <tiago7164@hotmail.com>
2023-09-05 23:56:18 +0100

    Implemented routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-// import Home from './components/pages/Home'
-// import Users from './components/pages/Users'
-// import UserBlog from './components/pages/UserBlog'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import Home from './components/pages/Home'
+import Users from './components/pages/Users'
+import UserBlog from './components/pages/UserBlog'
 import PostForm from './components/pages/PostForm'
 
 import "./styles/normalize.css"
@@ -8,7 +10,17 @@ import "./styles/fontawesome.min.css"
 import "./styles/main.css"
 
 function App() {
-  return <PostForm />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/users/:userId" element={<UserBlog />} />
+        <Route path="/users/:userId/post" element={<PostForm />} />
+        <Route path="*" element={<h1>Not Found</h1>} />
+        </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
