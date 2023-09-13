33d414e1e813e782ca005df5ec89799f411dbcca
“tiagoluis12” <tiago7164@hotmail.com>
2023-09-13 01:39:50 +0100

    Add collapsible menu

---
+++ src/components/molecules/DrawerMenu.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Drawer from '@mui/material/Drawer';
+
+
+export default function DrawerMenu(props) {
+  return (
+    <Drawer
+      anchor="right"
+      open={props.open}
+      onClose={() => props.setOpen(false)}
+    >
+      Hello World!
+    </Drawer>
+  )
+}
--- src/components/templates/Default.jsx
+++ src/components/templates/Default.jsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 import logo from "../../images/logo.svg";
+import DrawerMenu from "../molecules/DrawerMenu";
 
 export default function Default(props) {
+  const [open, setOpen] = useState(false);
+
   return (
     <div className="wrapper">
+      <DrawerMenu open={open} setOpen={setOpen} />
       <div className="app-header">
         <div className="app-header__logo">
           <img src={logo} className="responsive" alt="" />
         </div>
         <div className="app-header__menu">
-          <i className="fa fa-bars"></i>
+          <i onClick={() => setOpen(true)} className="fa fa-bars"></i>
         </div>
       </div>
       {props.children}
