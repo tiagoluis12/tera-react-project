dac99cb730d2397210d707d608db26d3dce3e9c1
“tiagoluis12” <tiago7164@hotmail.com>
2023-09-22 10:50:13 +0100

    Using CSS Modules to avoid CSS selector conflicts

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
-import logo from "../../images/logo.svg"
 import AppLoading from '../organisms/AppLoading';
+
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
+
+import logo from "../../images/logo.svg"
+
+import styles from "./Home.module.css"
+
 
 export default function Home() {
   const navigate = useNavigate();
 
+  const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
+
   const [users, setUsers] = useState([]);
   const [isLoading, setIsLoading] = useState(true)
-  const [currentUser, setCurrentUser] = useState("")
-
-    console.log("currentUser", currentUser);
-
 
   useEffect(() => {
     fetch("https://63cf09718a780ae6e6710dbe.mockapi.io/users")
@@ -28,11 +32,11 @@ export default function Home() {
   return isLoading ?(
     <AppLoading />
   ) : (
-    <div className="home center">
-    <div className="home__logo">
+    <div className={`${styles.home} center`}>
+    <div className={styles.logo}>
       <img src={logo} className="responsive" alt="" />
     </div>
-    <select onChange={handleUserChange} className="home__select-users">
+    <select onChange={handleUserChange} className={styles.homeSelectUsers}>
       <option value="">Seleione um usuario</option>
       {users.sort((a, b) => a.fn.localeCompare(b.fn)).map((user) => (<option value={user.id} key={user.id}>{user.fn} {user.ln}</option>))}
     </select>
